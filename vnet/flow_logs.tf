bc2e72d0005bfce223bc08c613364479cf7f5ce3
Nils Pinnau <nils.pinnau@live.de>
2025-02-13 13:48:34 +0100

    flow logs updated

--- vnet/flow_logs.tf
+++ vnet/flow_logs.tf
@@ -3,37 +3,31 @@ data "azurerm_network_watcher" "nw" {
   resource_group_name = "NetworkWatcherRG"
 }
 
-resource "azapi_resource" "vnet_flow_logs" {
-  count = var.flow_logs.enabled == true ? 1 : 0
+resource "azurerm_network_watcher_flow_log" "vnet_flow_logs" {
 
-  type      = "Microsoft.Network/networkWatchers/flowLogs@2023-11-01"
-  name      = "logs-${azurerm_virtual_network.default.name}"
-  location  = var.location
-  parent_id = data.azurerm_network_watcher.nw.id
+  name     = "logs-${azurerm_virtual_network.default.name}"
+  location = var.location
 
-  body = jsonencode({
-    properties = {
-      enabled = true
-      flowAnalyticsConfiguration = var.flow_logs.config.traffic_analytics.enabled ? {
-        networkWatcherFlowAnalyticsConfiguration = {
-          enabled             = var.flow_logs.config.traffic_analytics.enabled == true && var.flow_logs.config.traffic_analytics.workspace != null
-          workspaceId         = var.flow_logs.config.traffic_analytics.workspace.workspace_id
-          workspaceRegion     = var.flow_logs.config.traffic_analytics.workspace.location
-          workspaceResourceId = var.flow_logs.config.traffic_analytics.workspace.resource_id
-        }
-      } : {}
-      format = {
-        type    = "JSON"
-        version = 2
-      }
-      retentionPolicy = {
-        days    = 30
-        enabled = true
-      }
-      storageId        = var.flow_logs.config.storage_account_id
-      targetResourceId = azurerm_virtual_network.default.id
-    }
-  })
+  resource_group_name  = data.azurerm_network_watcher.nw.resource_group_name
+  network_watcher_name = data.azurerm_network_watcher.nw.name
+
+  target_resource_id = azurerm_virtual_network.default.id
+  storage_account_id = var.flow_logs.config.storage_account_id
+
+  enabled = var.flow_logs.enabled
+  version = 2
+
+  traffic_analytics {
+    enabled               = var.flow_logs.config.traffic_analytics.enabled
+    workspace_resource_id = var.flow_logs.config.traffic_analytics.workspace.resource_id
+    workspace_id          = var.flow_logs.config.traffic_analytics.workspace.workspace_id
+    workspace_region      = var.flow_logs.config.traffic_analytics.workspace.location
+  }
+
+  retention_policy {
+    enabled = true
+    days    = try(var.flow_logs.retention_policy_days, 14)
+  }
 
   tags = var.tags
 }
