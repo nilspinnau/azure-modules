1f5caacc62a273068a5421723bfbea864c86ffb1
Nils Pinnau <nils.pinnau@live.de>
2025-03-10 14:38:10 +0100

    storage azurerm proivder and diagnostic settings update

--- storage-account/monitor.tf
+++ storage-account/monitor.tf
@@ -1,9 +1,3 @@
-data "azurerm_monitor_diagnostic_categories" "default" {
-  count = var.monitoring.enabled == true ? 1 : 0
-
-  resource_id = azurerm_storage_account.default.id
-}
-
 
 resource "azurerm_monitor_diagnostic_setting" "default" {
   count = var.monitoring.enabled == true ? 1 : 0
@@ -13,10 +7,8 @@ resource "azurerm_monitor_diagnostic_setting" "default" {
   log_analytics_destination_type = "Dedicated"
   log_analytics_workspace_id     = var.monitoring.config.workspace.resource_id
 
-  dynamic "enabled_log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.default.0.log_category_types
-    content {
-      category = enabled_log.value
-    }
+  metric {
+    category = "Availability"
+    enabled  = true
   }
 }
--- storage-account/providers.tf
+++ storage-account/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.5.0"
+  required_version = ">=1.7.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.6.0"
+      version = ">=4.0"
     }
     random = {
       source  = "hashicorp/random"
