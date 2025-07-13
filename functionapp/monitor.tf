0ceac476d2551693989af50b5772c20e685188ae
Nils Pinnau <nils.pinnau@live.de>
2025-07-13 13:41:51 +0200

    appinsights in law name to avoid conflicts - rethink in the future

--- functionapp/monitor.tf
+++ functionapp/monitor.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_log_analytics_workspace" "default" {
-  name                = "law-${var.resource_suffix}"
+  name                = "law-appinsights-${var.resource_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = "PerGB2018"
