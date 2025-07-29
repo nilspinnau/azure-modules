2af34106aea9d0f67fbc2de41e00b91953842c74
Nils Pinnau <nils.pinnau@live.de>
2025-07-29 16:18:01 +0200

    vars for webdeploy, ftp deploy, vars for auth_settings (incomplete)

--- functionapp/linux.tf
+++ functionapp/linux.tf
@@ -16,11 +16,11 @@ resource "azurerm_linux_function_app" "default" {
   app_settings                  = var.app_settings
 
   https_only                    = true
-  public_network_access_enabled = true
+  public_network_access_enabled = var.public_network_access_enabled
   enabled                       = true
 
-  webdeploy_publish_basic_authentication_enabled = false
-  ftp_publish_basic_authentication_enabled       = true
+  webdeploy_publish_basic_authentication_enabled = var.webdeploy_publish_basic_authentication_enabled
+  ftp_publish_basic_authentication_enabled       = var.ftp_publish_basic_authentication_enabled
 
   site_config {
     always_on           = var.site_config.always_on
@@ -42,9 +42,17 @@ resource "azurerm_linux_function_app" "default" {
     application_insights_key                = azurerm_application_insights.default.instrumentation_key
   }
 
+  auth_settings_v2 {
+    auth_enabled           = var.auth_settings_v2.auth_enabled
+    require_authentication = var.auth_settings_v2.require_authentication
+    require_https          = var.auth_settings_v2.require_https
+
+    login {}
+  }
+
   zip_deploy_file = var.zip_deploy_file
 
-  storage_account_name       = var.storage_account.name
+  storage_account_name = var.storage_account.name
 
   virtual_network_subnet_id = var.subnet_id
 }
--- functionapp/variables.tf
+++ functionapp/variables.tf
@@ -22,6 +22,39 @@ variable "service_plan_id" {
 
 }
 
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "Whether public network access is enabled for the Function App."
+  default     = false
+}
+
+variable "webdeploy_publish_basic_authentication_enabled" {
+  type        = bool
+  description = "Enable basic authentication for Web Deploy."
+  default     = false
+}
+
+variable "ftp_publish_basic_authentication_enabled" {
+  type        = bool
+  description = "Enable FTP publish basic authentication."
+  default     = false
+}
+
+variable "auth_settings_v2" {
+  type = object({
+    auth_enabled           = optional(bool, false)
+    require_authentication = optional(bool, false)
+    require_https          = optional(bool, true)
+    active_directory = optional(object({
+    }), {})
+    google = optional(object({
+    }), {})
+    microsoft = optional(object({
+    }), {})
+  })
+  default = {}
+}
+
 variable "os_type" {
   type        = string
   description = "The OS type of the App Service Plan."
